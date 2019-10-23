803ed149ccf69a8cbd67c634d613d31afd300abe
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-23 14:41:31 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,4 +1,12 @@
-class FancyDance
-  include Dance
-
+module FancyDance
+  module InstanceMethods              # you "include" these instance methods
+    def superjump
+      "I'm just one jumping instance"
+    end
+  end
+  module ClassMethods                 # you "extend" these class methods
+    def megajump
+      "I'm a whole class of MOFO's jumping"
+    end
+  end
 end
